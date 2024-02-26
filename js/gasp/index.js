29044862eda2ffc35a6b46571bcaf83b00044444
Ghassan <66538932+San29n@users.noreply.github.com>
2024-02-26 20:38:38 +0300

    Delete js directory

--- js/gasp/index.js
+++
@@ -1,6 +0,0 @@
-import { CSSPlugin } from "./CSSPlugin.js";
-import { Back, Bounce, Circ, Cubic, Elastic, Expo, Linear, Power0, Power1, Power2, Power3, Power4, Quad, Quart, Quint, Sine, SteppedEase, Strong, TimelineLite, TimelineMax, TweenLite, gsap } from "./gsap-core.js";
-var gsapWithCSS = gsap.registerPlugin(CSSPlugin) || gsap,
-    // to protect from tree shaking
-TweenMaxWithCSS = gsapWithCSS.core.Tween;
-export { Back, Bounce, CSSPlugin, Circ, Cubic, Elastic, Expo, Linear, Power0, Power1, Power2, Power3, Power4, Quad, Quart, Quint, Sine, SteppedEase, Strong, TimelineLite, TimelineMax, TweenLite, TweenMaxWithCSS as TweenMax, gsapWithCSS as default, gsapWithCSS as gsap };
